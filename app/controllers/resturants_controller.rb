fdab2c5e20beaff1849b9c2e0527da5450e4b760
dapmas <sampadmedda@gmail.com>
2017-05-15 18:17:38 +0530

    Created associations amon table. Ver 1

--- app/controllers/resturants_controller.rb
+++ app/controllers/resturants_controller.rb
@@ -2,4 +2,11 @@ class ResturantsController < ApplicationController
   def index
     @resturants = Resturant.all
   end
+
+  def show
+    @resturant = Resturant.find(params[:id])
+    @title = @resturant.name
+    @dishes = @resturant.dishes
+    #@disable_pagination = true
+  end
 end
