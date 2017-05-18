9aff47cf6d7f96ccfa0ada150744e9d38032252f
dapmas <sampadmedda@gmail.com>
2017-05-19 01:20:46 +0530

    Added CRUD functionalities to resturant resource

--- app/controllers/resturants_controller.rb
+++ app/controllers/resturants_controller.rb
@@ -5,8 +5,45 @@ class ResturantsController < ApplicationController
 
   def show
     @resturant = Resturant.find(params[:id])
-    @title = @resturant.name
-    @ingredients = @resturant.ingredients
-    #@disable_pagination = true
+    @ingredients = Ingredient.all
+
   end
+
+  def new
+		@resturant = Resturant.new
+	end
+
+	def create
+		@resturant = Resturant.new(resturant_params)
+		if @resturant.save
+			redirect_to resturants_path, :notice => "This resturant has been added to our records."
+		else
+			render "new"
+		end
+	end
+
+	def edit
+		@resturant = Resturant.find(params[:id])
+	end
+
+	def update
+		@resturant = Resturant.find(params[:id])
+		if @resturant.update_attributes(resturant_params)
+			redirect_to resturant_path, :notice => "This resturant has been updated in our records."
+		else
+			render "edit"
+		end
+	end
+
+	def destroy
+		@resturant = Resturant.find(params[:id])
+		@resturant.destroy
+		redirect_to resturants_path, :notice => "This Resturant has been deleted."
+	end
+
+	private
+
+	def resturant_params
+    params.require(:resturant).permit(:name, ingredient_ids: [])
+	end
 end
