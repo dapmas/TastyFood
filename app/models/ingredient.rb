244e6d5d50a8c144d26264d4c2893483cf36ed76
dapmas <sampadmedda@gmail.com>
2017-05-19 10:45:20 +0530

    Added authorization to Ingredients resource

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,4 +3,5 @@ class Ingredient < ActiveRecord::Base
   has_many :dishes, :through => :meals
   has_many :inventories, dependent: :destroy
   has_many :resturants, :through => :inventories
+  resourcify
 end
