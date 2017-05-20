1c22dcf56be0598cfe741503bc902159ffadc0b8
dapmas <sampadmedda@gmail.com>
2017-05-20 11:55:45 +0530

    clean master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170518102336) do
+ActiveRecord::Schema.define(version: 20170520061223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20170518102336) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text     "body"
+    t.integer  "user_id"
+    t.integer  "owner_id"
   end
 
   create_table "dishes_ingredients", id: false, force: :cascade do |t|
@@ -41,8 +43,8 @@ ActiveRecord::Schema.define(version: 20170518102336) do
   create_table "inventories", force: :cascade do |t|
     t.integer  "resturant_id"
     t.integer  "ingredient_id"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "meals", force: :cascade do |t|
@@ -59,17 +61,11 @@ ActiveRecord::Schema.define(version: 20170518102336) do
     t.datetime "updated_at",   null: false
   end
 
-  create_table "recipies", force: :cascade do |t|
-    t.integer  "ingredient_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.integer  "dish_id"
-  end
-
   create_table "resturants", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
 end
